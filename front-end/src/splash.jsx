4c893de703d859eda4aa5ffe04b44dbaa6a3e254
Joshua Strzeszkowski <joshstrzeszkowski@gmail.com>
2018-11-06 16:27:25 -0600

    added link to sign up from splash page

--- front-end/src/splash.jsx
+++ front-end/src/splash.jsx
@@ -3,9 +3,28 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 export default class Splash extends Component {
 
+  constructor(props) {
+    super(props);
+    this.redirectToLogin = this.redirectToLogin.bind(this);
+    this.redirectToSignUp = this.redirectToSignUp.bind(this);
+  }
+
+  redirectToLogin() {
+    this.props.history.push('/login');
+  }
+
+  redirectToSignUp() {
+    this.props.history.push('/sign_up');
+  }
+
   render() {
     return (
-      <div>Splash page</div>
+      <div>
+        <div>Splash page</div>
+        <div>
+          <button onClick={this.redirectToSignUp}>Sign Up</button>
+        </div>
+      </div>
     )
   }
 }
